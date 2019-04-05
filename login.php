ffc1409ad8f422dca4d18b0b3991b94aaec93b97
Gabriel28 <nasc.gabriel.1997@gmail.com>
2019-04-05 01:53:11 -0300

    Forms, botões, lembrar-me, esqueci a senha

---
+++ login.php
@@ -0,0 +1,63 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - BRAZIL GAMING WORLD</title>
+    
+    <link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--CSS personalizado styles-->
+	<link rel="stylesheet" type="text/css" href="css/style-login.css">
+</head>
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Entrar</h3>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="login" class="form-control" placeholder="Username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" name="pass" class="form-control" placeholder="Senha">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox" name="lembrar">Lembrar-me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Não tem conta? <a href="#"> Cadastre-se!</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Esqueci minha senha</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
